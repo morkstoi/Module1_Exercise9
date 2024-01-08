c544c8c7e88954976ca0448a7fbe240dff5e21ef
theac <theac@DESKTOP-LEARA2L>
2024-01-08 18:28:01 +0300

    Add project files.

---
+++ CalculatorWithException/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorWithException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Введите целое число:");
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите целое число:");
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите код операции:\n1 - сложение\n2 - вычитание\n3 - произведение\n4 - частное");
+                int operationCode = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Выбранный тип операции: {0}", operationCode);
+                double result = 0;
+                switch(operationCode)
+                {
+                    case 1:
+                        result = firstNumber + secondNumber;
+                        break;
+                    case 2:
+                        result = firstNumber - secondNumber;
+                        break;
+                    case 3:
+                        result = firstNumber * secondNumber;
+                        break;
+                        
+                    case 4:
+                        if (secondNumber == 0)
+                        {
+                            throw new DivideByZeroException();
+                        }
+                        result = (double)firstNumber / secondNumber;
+                        break;
+                    default:
+                        throw new InvalidOperationException("Нет операции с указанным кодом");
+                }
+                Console.WriteLine("Результат = {0,1:f2}",result);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Деление на ноль невозможно");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ CalculatorWithException/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CalculatorWithException")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorWithException")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("98f13c8c-4e52-4ddf-9631-7045d0f4e134")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
